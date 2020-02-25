58a20d0de0866990eebc68d9820f46b21137f57f
kiran <gandikotakiran99@gmail.com>
2020-02-25 15:26:13 +0530

    added the hovercounter2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-// import ClickCounter from './components/ClickCounter';
-import HookCounter from './components/HookCounter';
+import ClickCounter from './components/ClickCounter';
+// import HookCounter from './components/HookCounter';
+import HookCounter2 from './components/HookCounter2';
 
 function App() {
   return (
     <div className="App">
-      {/* <ClickCounter/> */}
-      <HookCounter/>
+      <ClickCounter/>
+      {/* <HookCounter/> */}
+      <HookCounter2/>
     </div>
   );
 }
---
+++ src/components/HookCounter2.js
@@ -0,0 +1,19 @@
+import React,{useState} from 'react'
+
+function HookCounter2() {
+    const initialCount=0;
+    const [count,setCount]=useState(initialCount);
+    return (
+        <div>
+            Count: {count}
+            {/* <button onClick={()=>setCount(initialCount)}>Reset</button>
+            <button onClick={()=>setCount(count+1)}>Increment</button>
+            <button onClick={()=>setCount(count-1)}>Decrement</button> */}
+
+            <button onClick={()=>setCount(initialCount)}>Reset</button>
+            <button onClick={()=>{setCount(prevCount=>prevCount+1)}}>Increment</button>
+            <button onClick={()=>setCount(prevCount=>prevCount-1)}>Decrement</button>
+        </div>
+    )
+}
+export default HookCounter2
