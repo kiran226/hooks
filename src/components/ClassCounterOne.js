fd7e9aadff3136eccf846dfc164b168a5f03b0de
kiran <gandikotakiran99@gmail.com>
2020-02-25 17:49:49 +0530

    useEffect hook for conditionally rendering

--- src/components/ClassCounterOne.js
+++ src/components/ClassCounterOne.js
@@ -5,15 +5,20 @@ class ClassCounterOne extends Component {
         super(props)
     
         this.state = {
-             count:0
+             count:0,
+             name:''
         }
     }
     componentDidMount(){
         document.title=`You clicked ${this.state.count} times`
     }
 
-    componentDidUpdate(){
-        document.title=`You clicked ${this.state.count} times`
+    componentDidUpdate(prevProps,prevState){
+        if(prevState.count!==this.state.count){
+            console.log('updating the document title');
+            document.title=`You clicked ${this.state.count} times`
+        }
+        
     }
     
     incrementCount=()=>{
@@ -22,11 +27,14 @@ class ClassCounterOne extends Component {
         })
     }
 
-    
     render() {
         const {count}=this.state;
         return (
             <div>
+                <input type="text" 
+                value={this.state.name} 
+                onChange={e=>{this.setState({name: e.target.value} )}}
+                />
                <button onClick={this.incrementCount}>Clicked {count} times</button> 
             </div>
         )
--- src/components/HookCounterOne.js
+++ src/components/HookCounterOne.js
@@ -2,11 +2,15 @@ import React,{useState,useEffect} from 'react'
 
 function HookCounterOne() {
     const[count,setCount]=useState(0);
+    const [name,setName]=useState('');
     useEffect(()=>{
         document.title=`You clicked ${count} times`
-    })
+        console.log('updating the document title')
+    },[count])
     return (
         <div>
+            <input type="text"
+              value={name} onChange={e=>setName(e.target.value)}/>
             <button onClick={()=>setCount(count+1)}>Click {count} times</button>
         </div>
     )
