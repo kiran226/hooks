926433379671e416aa36024a5e152f9ab39f835e
kiran <gandikotakiran99@gmail.com>
2020-02-25 14:47:20 +0530

    clickcounter and hookcounter

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+// import ClickCounter from './components/ClickCounter';
+import HookCounter from './components/HookCounter';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ClickCounter/> */}
+      <HookCounter/>
+    </div>
+  );
+}
+
+export default App;
