8ad19b3845ae39abde36445f560df88010aa874b
kiran <gandikotakiran99@gmail.com>
2020-02-26 10:35:52 +0530

    useEffect hook and fetched the data on button click

--- src/DataFetching.js
+++ src/DataFetching.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 function DataFetching() {
     const [post,setPost]=useState({});
     const[id,setId]=useState(1);
+    const[idFromButtonClick,setIdFromButtonClick]= useState(1);
     useEffect(()=>{
         axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
         .then((res)=>{
@@ -13,10 +14,15 @@ function DataFetching() {
         .catch((err)=>{
             console.log(err)
         })
-    },[id])
+    },[idFromButtonClick]);
+
+    const handleSubmit=()=>{
+        setIdFromButtonClick(id);
+    }
     return (
         <div>
         <input type="text" value={id} onChange={e=>setId(e.target.value)}/>
+        <button type="button" onClick={handleSubmit}>Fetch Post</button>
         <div>{post.title}</div>
 
             {/* <ul>
